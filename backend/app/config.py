296bac90ca15425f7a33a71c3d1688119fc02e2b
GabeMed <gabrymedeiros7@gmail.com>
2025-06-05 23:59:32 -0300

    Add config

---
+++ backend/app/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    DATABASE_URL: str
+    OPENAI_API_KEY: str
+    UPLOAD_DIR: str = "./TEMP_FILES"
+    AI_OUTPUT_DIR: str = "./AI_OUTPUT"
+
+    class Config:
+        env_file = ".env"
